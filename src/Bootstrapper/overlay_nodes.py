b149c369ba60e13d13b2839e90096a5992d97753
RuiRodrigues17 <ruimsrodrigues2004@gmail.com>
2025-11-03 22:18:44 +0000

    neste commit criei a estrutura para o trabalho e criei a logica do bootsrapper e do node

---
+++ src/Bootstrapper/overlay_nodes.py
@@ -0,0 +1,7 @@
+# overlay_nodes.py
+node_overlay = {
+    "node_ip": ["node_ip2", "node_ip3"],
+    "node_ip2": ["node_ip", "node_ip4"],
+    "node_ip3": ["node_ip"],
+    "node_ip4": ["node_ip2"]
+}

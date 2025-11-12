6f1354b8f0702a3b837a34d15c536e1a6f2cbb0e
xaloftal <dianaacdinis@gmail.com>
2025-11-12 18:02:58 +0000

    feat: started the logic for packages handlers, as well as a message aux file

--- src/Node/control_server.py
+++ src/Node/control_server.py
@@ -8,47 +8,19 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from Node.node import Node
-import Bootstrapper.overlay_nodes as nodes
+from Proto.aux_message import MessageType, create_message, parse_message, get_message_type
+
 
 class Server(Node):
   
-  def __init__(self, node_id, node_ip, bootstrapper_ip, bootstrapper_port,
-               flood_port=6000, stream_port=1000, alive_port=2000):
-      super().__init__(node_id, node_ip, bootstrapper_ip, bootstrapper_port,
-                      flood_port, stream_port, alive_port) 
+  def __init__(self, node_id, node_ip, bootstrapper_ip,bootstrapper_port = 1000, tcpport = 6000, udpport = 7000, videosrc):
+      super().__init__(node_id, node_ip, bootstrapper_ip, bootstrapper_port, tcpport, udpport) 
+      self.videosrc = videosrc
 
 
-  def start(self):
-      server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-      server.bind((self.node_ip, self.control_port))
-      server.listen()
-      print(f"[{self.node_id}] listening on {self.node_ip}:{self.control_port}")
-
-      while True:
-          conn, addr = server.accept()
-          threading.Thread(target=self.handle_client, args=(conn, addr), daemon=True).start()
-
-  def handle_client(self, conn, addr):
-        try:
-            data = conn.recv(1024).decode()
-            if data:
-                print(f"[{self.node_id}] recebeu de {addr}: {data}")
-                
-                # Try to parse as JSON message
-                try:
-                    msg = json.loads(data)
-                    if msg.get('type') == 'FLOOD':
-                        # This is a flood message coming back (shouldn't happen for server)
-                        print(f"[{self.node_id}] Received flood message (unexpected for server)")
-                except json.JSONDecodeError:
-                    # Not a JSON message, handle as plain text
-                    pass
-                    
-        except Exception as e:
-            print(f"[{self.node_id}] erro a receber: {e}")
-        finally:
-            conn.close()
+    #
+    # FLOOD METHODS
+    # 
     
     def initiate_flood(self):
         """
@@ -56,32 +28,54 @@ class Server(Node):
         This should be called by the server when it wants nodes to update their routes.
         """
         flood_id = str(uuid.uuid4())  # Unique ID for this flood
-        flood_msg = {
-            'flood_id': flood_id,
-            'origin_id': self.node_id,
-            'origin_ip': self.node_ip,
-            'hop_count': 1  # Starting at 1 for first hop
-        }
-        
         print(f"[{self.node_id}] Initiating flood with ID: {flood_id}")
+
+        for neighbor_ip in self.neighbors:      
+            msg = create_message(msg_id = flood_id, 
+                                msg_type = MessageType.FLOOD,
+                                srcip = self.node_ip,
+                                destip = neighbor_ip,
+                                data={
+                                    # TODO: change this into a metric, like bamdwidth
+                                    'hop_count': 0
+                                })
+            self.forward_flood_message(msg)
         
         # Add to our own flood cache to avoid processing it if it comes back
         with self.lock:
-            self.flood_cache.add(flood_id)
-        
-        # Send to all neighbors
-        self.forward_flood_message(flood_msg, exclude_ip=None)
+            self.flood_cache.add(flood_id)       
         
         print(f"[{self.node_id}] Flood initiated and sent to neighbors")
     
+    
+    
+    #
+    # DEPLOYABLE METHODS
+    #
     def manual_flood(self):
         """
         Manually trigger a flood message.
-        Can be called from CLI or other interface.
         """
         print(f"\n[{self.node_id}] === Manual Flood Triggered ===")
         self.initiate_flood()
     
 
+    #
+    # AUXILIARY METHODS
+    #
+    
+    def forward_flood_message(self, msg):
+        """
+        Forwards a flood message to a neighbor.
+        """
+        dest_ip = msg['destip']
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((dest_ip, self.tcpport))
+            sock.send(json.dumps(msg).encode())
+            sock.close()
+            print(f"[{self.node_id}] Forwarded FLOOD to {dest_ip}")
+        except Exception as e:
+            print(f"[{self.node_id}] Error forwarding FLOOD to {dest_ip}: {e}")
     
     

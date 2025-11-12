b306cae0267feaea2bddf608b58efb782d78432e
Goncaloefm11 <goncaloefmagalhaes@gmail.com>
2025-11-12 22:42:13 +0000

    1

--- src/Bootstrapper/overlay_nodes.py
+++ src/Bootstrapper/overlay_nodes.py
@@ -1,6 +1,6 @@
 # overlay_nodes.py
 node_overlay = {
-    "10.0.1.2": ["10.0.1.1", "10.0.4.1"],
+    "10.0.1.2": ["10.0.3.2", "10.0.4.1"],
     "node_ip2": ["node_ip", "node_ip4"],
     "node_ip3": ["node_ip"],
     "node_ip4": ["node_ip2"]

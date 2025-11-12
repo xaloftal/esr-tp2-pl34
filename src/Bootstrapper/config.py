64bdcb04a8f95876f07d0a33668c06a1150be4a7
Goncaloefm11 <goncaloefmagalhaes@gmail.com>
2025-11-12 22:44:02 +0000

    Update config.py

--- src/Bootstrapper/config.py
+++ src/Bootstrapper/config.py
@@ -1,3 +1,3 @@
-HOST = 'localhost'  # Ip do Bootstrapper
+HOST = '10.0.18.21'  # Ip do Bootstrapper
 PORT =  5000  # Porta do Bootstrapper
 N_vizinho = 3  # Numero de vizinhos que cada nodo deve ter
